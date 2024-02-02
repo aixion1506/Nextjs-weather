b3fc10aa568efe6001bfda18c82b47c2270d290f
aixion <aixion1506@gmail.com>
2024-02-02 17:41:26 +0900

    다이나믹 라우팅 구현

---
+++ src/app/[location]/page.tsx
@@ -0,0 +1,16 @@
+
+type Props = {
+    params: {
+    location: string
+    }
+}
+
+export default function Detail({ params }: Props) {
+    const name = params.location === 'seoul' ? '서울' : ''
+
+    return(
+        <>
+            <h1> {name}의 3일의 예보</h1>
+        </>
+    )
+}
