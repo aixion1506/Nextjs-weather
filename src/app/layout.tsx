af0ce3feebc0610cfb5e453f56a3e26b60f61982
aixion <aixion1506@gmail.com>
2024-02-05 15:21:40 +0900

    지역, 전역으로 스타일 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
+import './global.css'
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-    </html>
+    </html> 
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 
+import style from './style.module.css'
 
 export default function Home() {
   return (
     <>
     <h1> 날씨 앱 </h1>
-    <ul>
+    <ul className={style.list}>
       <li>
         <Link href="/seoul">서울</Link>
       </li>
@@ -16,6 +17,7 @@ export default function Home() {
         <Link href="/london">런던</Link>
       </li>
     </ul>
+    <button>test</button>
    </>
   );
 }
